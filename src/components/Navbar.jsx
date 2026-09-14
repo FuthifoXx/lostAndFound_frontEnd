f45ad23f5919ca0498f038acbe627c0a96f40cbf
FuthifoXx <71636403+FuthifoXx@users.noreply.github.com>
2026-09-14 21:24:37 +0200

    Polish login navigation action

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Logo from './Logo'
 import { getRoleHome } from '../utils/getRoleHome'
 
 function Navbar() {
   const user = JSON.parse(localStorage.getItem('user'))
+  const { pathname } = useLocation()
+  const isLoginPage = pathname === '/login'
 
   return (
     <nav className='nav'>
       <div className='nav-center'>
         <Logo />
         <div className='nav-links' aria-label='Primary navigation'>
-          <Link to='/items'>Browse Items</Link>
+          <Link
+            to='/items'
+            className={`nav-browse${isLoginPage ? ' nav-login' : ''}`}
+          >
+            Browse Items
+          </Link>
 
-          {!user && <Link to='/login' className='nav-login'>Login</Link>}
+          {!user && !isLoginPage && (
+            <Link to='/login' className='nav-login'>Login</Link>
+          )}
 
           {user && (
             <Link to={getRoleHome(user.role)} className='nav-login'>Dashboard</Link>
