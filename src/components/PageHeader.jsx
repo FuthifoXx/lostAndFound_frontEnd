4f13568c4725e2ccdf2441aa96951acf9293b308
FuthifoXx <71636403+FuthifoXx@users.noreply.github.com>
2026-09-17 09:55:01 +0200

    Center shared page headers

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,6 +2,7 @@ function PageHeader({ title, description }) {
   return (
     <header className='page-header'>
       <h1>{title}</h1>
+      <div className='title-underline' aria-hidden='true'></div>
       {description && <p>{description}</p>}
     </header>
   )
